16cfc0b05f3f84c69a270cf633d9bcd23cb8811e
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2023-04-08 13:35:25 +0530

    connected the project to the backend

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,53 +1,103 @@
-import Card from '../UI/Card'
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Chole Bature',
-      description: 'Spicy Chole with delicious Bature',
-      price: 150.00,
-    },
-    {
-      id: 'm2',
-      name: 'Paneer Tikka',
-      description: 'Too delicious',
-      price: 200.00,
-    },
-    {
-      id: 'm3',
-      name: 'Zinger Burger',
-      description: 'Crispy and tasty',
-      price: 179.99,
-    },
-    {
-      id: 'm4',
-      name: 'Vegetable Sandwich',
-      description: 'Healthy...and green...',
-      price: 60.00,
-    },
-];
+// const DUMMY_MEALS = [
+//     {
+//       id: 'm1',
+//       name: 'Chole Bature',
+//       description: 'Spicy Chole with delicious Bature',
+//       price: 150.00,
+//     },
+//     {
+//       id: 'm2',
+//       name: 'Paneer Tikka',
+//       description: 'Too delicious',
+//       price: 200.00,
+//     },
+//     {
+//       id: 'm3',
+//       name: 'Zinger Burger',
+//       description: 'Crispy and tasty',
+//       price: 179.99,
+//     },
+//     {
+//       id: 'm4',
+//       name: 'Vegetable Sandwich',
+//       description: 'Healthy...and tasty...',
+//       price: 60.00,
+//     },
+// ];
 
 const AvailableMeals = () => {
-    
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem
-        id={meal.id} // this is new!
-        key={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-    />)
+  const [meals, setMeals] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
+  useEffect(() => {
+    const fetchMeal = async () => {
+      const response = await fetch(
+        "https://react-http-b38c1-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setIsLoading(false);
+      setMeals(loadedMeals);
+    };
+
+    fetchMeal().then().catch((error) => {
+       setIsLoading(false);
+       setHttpError(error.message);
+    });
+  }, []);
+
+  if (isloading) {
     return (
-        <section className={classes.meals}>
-            <Card>
-            <ul>
-               {mealsList}
-            </ul>
-            </Card>
-        </section>
-    )
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+if(httpError){
+  return <section className={classes.MealsError}>
+     <h1>{httpError}</h1>
+  </section>
 }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id} // this is new!
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
 
 export default AvailableMeals;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -63,6 +63,10 @@ if(action.type === 'REMOVE'){
     }
 }
 
+if(action.type === 'CLEAR'){
+    return defaultCartState
+}
+
 return defaultCartState;
 
 }
@@ -78,11 +82,16 @@ const CartProvider = (props) => {
         dispatchCartAction({type:'REMOVE' , id:id})
     };
 
+    const clearCartHandler = () => {
+        dispatchCartAction({type: 'CLEAR'})
+    }
+
     const cartContext = {
         items:cartState.items,
         totalAmount:cartState.totalAmount,
         addItem:addItemtoCartHandler,
-        removeItem:removeItemFromCardHandler
+        removeItem:removeItemFromCardHandler,
+        clearCart:clearCartHandler
     }
 
     return (
